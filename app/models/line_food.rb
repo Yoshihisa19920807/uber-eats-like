df5d38302ca0e6583dcbc836dea3a49fb42455c3
y-okada-spice <yoshihisa_okada@spice-factory.co.jp>
2022-04-07 15:37:54 +0900

    create models

---
+++ app/models/line_food.rb
@@ -0,0 +1,13 @@
+class LineFood < ActiveRecord
+  belongs_to :food
+  belongs_to :order, optional: true
+
+  validates :count, presence: true
+
+  scope :active, -> { where(is_active: true) }
+  scope :picked_restaurant, -> (picked_restaurant_id) { where.not(restaurant_id: picked_restaurant_id) }
+
+  def total_amount
+    food.price * count
+  end
+end
---
+++ app/models/order.rb
@@ -0,0 +1,15 @@
+class Order < ActiveRecord
+  has_many :line_foods
+
+  validates :total_price, numerically: { greater_than: 0 }
+
+  # コントローラでやればいいのでは？
+  def save_with_update_line_foods!(line_foods)
+    ActiveRecord::Base.transaction do
+      line_foods.each do |line_food|
+        line_food.update(is_active: false, order: self)
+      end
+      self.save!
+    end
+  end
+end
