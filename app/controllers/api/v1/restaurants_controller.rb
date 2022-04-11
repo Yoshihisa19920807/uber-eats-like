6ba1941d1a30e958c1ca9dd826f93362607ebc4d
y-okada-spice <yoshihisa_okada@spice-factory.co.jp>
2022-04-11 15:34:59 +0900

    mod restaurants_controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,12 +1,8 @@
-module Api
-  module V1
-    class RestaurantsController < ApplicationController
-      def index
-        restaurants = Restaurant.all
-        render json: {
-          restaurants: restaurants
-        }, status: :ok
-      end
-    end
+class Api::V1::RestaurantsController < ApplicationController
+  def index
+    restaurants = Restaurant.all
+    render json: {
+      restaurants: restaurants
+    }, status: :ok
   end
 end
