a7d5eb9b7a99b5ad2f1275ad8c0dc703f7f61e9e
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-07-05 09:37:21 +0900

    mod foods index

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,25 +1,48 @@
-import React, { Fragment, useEffect } from 'react';
-
+import React, { Fragment, useEffect, useReducer } from 'react';
+// reducers
+import {
+  initialState as foodsInitialState,
+  foodsActionTypes,
+  foodsReducer,
+} from '../reducers/foods';
 // apis
 import { fetchFoods } from '../apis/foods';
+// constants
+import { REQUEST_STATE } from '../constants';
 
 export const Foods = ({
   match
 }) => {
-
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
   useEffect(() => {
-    fetchFoods(1)
-    .then((data) =>
-      console.log(data)
-    )
+    dispatch({ type: foodsActionTypes.FETCHING });
+    fetchFoods(match.params.restaurantId)
+      .then((data) => {
+        dispatch({
+          type: foodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
+      });
   }, [])
 
   return (
     <Fragment>
-      フード一覧
-      <p>
-        restaurantsIdは {match.params.restaurantId} です
-      </p>
+      {
+        foodsState.fetchState === REQUEST_STATE.LOADING ?
+          <Fragment>
+            <p>
+              ロード中...
+            </p>
+          </Fragment>
+        :
+          foodsState.foodsList.map(food =>
+            <div key={food.id}>
+              {food.name}
+            </div>
+          )
+      }
     </Fragment>
   )
 }
